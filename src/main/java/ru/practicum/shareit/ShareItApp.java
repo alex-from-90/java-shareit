a8614c7fbb48bf4c322b0ffb9d22431f14509ef9
alex-from-90 <alex@site-n.ru>
2023-07-26 02:42:59 +0300

    Add ManyToOne

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShareItApp {
-	/*С таким набором информации по учёбе - это тихий ужас...
-    Не уверен что правильно понял тему... Вообще, насколько понимаю, в чистом виде такое редко используется
-    Просьба сильно не мучать, если конечно всё не совсем плохо)). */
-	public static void main(String[] args) {
-		SpringApplication.run(ShareItApp.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItApp.class, args);
+    }
 }
